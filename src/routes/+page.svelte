c76f549ac82c5d0f91c84770932e59c6ccc0169c
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-07-12 12:05:58 -0400

    credits placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
             <li><a data-sveltekit-reload href="/evidence/">How Do We Know?</a></li>
             <li><a data-sveltekit-reload href="/topics/">Topics &amp; Ideas</a></li>
             <li><a data-sveltekit-reload href="/maps/">Where in the World?</a></li>
-            <li><a data-sveltekit-reload href="/">When?</a></li>
+            <li><a data-sveltekit-reload href="/when/">When?</a></li>
             <li>&nbsp;</li>
-            <li><a data-sveltekit-reload href="/">Credits</a></li>
+            <li><a data-sveltekit-reload href="/credits/">Credits</a></li>
         </ul>
 
         <p class="wireframe-note">WIREFRAME ONLY - NOT YET DESIGNED<br>
---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import MainNav from "$lib/MainNav.svelte";
+  // export let data;
+</script>
+
+<section class="wireframe-wrapper">
+<header id="header">
+
+  <MainNav />
+
+  <!-- Can this be outside of header -->
+  <h1>Credits</h1>
+</header>
+
+<section class="wrapper">
+<p class="wireframe-note">WIREFRAME ONLY - NOT YET DESIGNED</p>
+  
+<div class="feature-menu maps">
+    <ul class="col-heads">
+      <li class="feature-blurb">Thanks to..</li>
+    </ul>
+
+    <ul class="col-content">
+      <li class="feature-blurb">Credit number 1</li>
+    </ul>
+    <ul class="col-content">
+      <li class="feature-blurb">Credit number 2</li>
+    </ul>
+
+  </div>
+
+</section><!-- /wrapper -->
+</section><!-- /wireframe-wrapper -->
